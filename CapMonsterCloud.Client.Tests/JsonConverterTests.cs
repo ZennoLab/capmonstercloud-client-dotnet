4577cac56ca747e5b3f1e57bd0a2a90d92bc4481
Mateus Zanini <mateus-zanini@hotmail.com>
2022-10-28 15:44:35 -0300

    Changed test methods structure to match AAA pattern.

--- CapMonsterCloud.Client.Tests/JsonConverterTests.cs
+++ CapMonsterCloud.Client.Tests/JsonConverterTests.cs
@@ -13,7 +13,7 @@ namespace Zennolab.CapMonsterCloud.Client
         public void NumericFlagConverter_ShouldDeserialize([Values] bool numeric)
         {
             // Arrange
-            var target = new ImageToTextRequest
+            var request = new ImageToTextRequest
             {
                 Body = "some base64 body",
                 CapMonsterModule = CapMonsterModules.YandexWave,
@@ -23,41 +23,41 @@ namespace Zennolab.CapMonsterCloud.Client
                 Math = false
             };
 
+            var target = JsonConvert.SerializeObject(request);
+
             // Act
-            var actual = JsonConvert.SerializeObject(target);
-            var result = JsonConvert.DeserializeObject<ImageToTextRequest>(actual);
+            var actual = JsonConvert.DeserializeObject<ImageToTextRequest>(target);
 
             // Assert
-            result.Numeric.Should().Be(target.Numeric);
+            _ = actual.Numeric.Should().Be(request.Numeric);
         }
 
         [Test]
         public void DictionaryToSemicolonSplittedStringConverter_ShouldDeserialize()
         {
             // Arrange
-            var cookies = new Dictionary<string, string>
-            {
-                { "cookieA", "value#A" },
-                { "cookieB", "value#B" }
-            };
-
-            var target = new HCaptchaProxylessRequest
+            var request = new HCaptchaProxylessRequest
             {
                 WebsiteUrl = "https://lessons.zennolab.com/captchas/hcaptcha/?level=easy",
                 WebsiteKey = "472fc7af-86a4-4382-9a49-ca9090474471",
                 Invisible = false,
                 Data = "some data",
                 UserAgent = "PostmanRuntime/7.29.0",
-                Cookies = cookies
+                Cookies = new Dictionary<string, string>
+                {
+                    { "cookieA", "value#A" },
+                    { "cookieB", "value#B" }
+                }
             };
 
+            var target = JsonConvert.SerializeObject(request);
+
             // Act
-            var actual = JsonConvert.SerializeObject(target);
-            var result = JsonConvert.DeserializeObject<HCaptchaProxylessRequest>(actual);
+            var actual = JsonConvert.DeserializeObject<HCaptchaProxylessRequest>(target);
 
             // Assert
-            result.Cookies.Should().ContainKeys(cookies.Keys);
-            result.Cookies.Should().ContainValues(cookies.Values);
+            _ = actual.Cookies.Should().ContainKeys(request.Cookies.Keys);
+            _ = actual.Cookies.Should().ContainValues(request.Cookies.Values);
         }
 
         [Test]
