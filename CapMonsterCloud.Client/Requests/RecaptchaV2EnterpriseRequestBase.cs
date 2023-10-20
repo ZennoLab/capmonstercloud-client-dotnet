ad526f49e6ef4328ee7d93ee133619942ad6e4a4
alexander drozdov <alexander.drozdov@zennolab.com>
2023-10-20 15:39:28 +0300

    EnterprisePayload MinimumLength attribute reverted

--- CapMonsterCloud.Client/Requests/RecaptchaV2EnterpriseRequestBase.cs
+++ CapMonsterCloud.Client/Requests/RecaptchaV2EnterpriseRequestBase.cs
@@ -37,7 +37,7 @@ namespace Zennolab.CapMonsterCloud.Requests
         /// Send it to the API, so that we render the Recaptcha widget with this parameter properly.
         /// </summary>
         [JsonProperty("enterprisePayload")]
-        [StringLength(int.MaxValue)]
+        [StringLength(int.MaxValue, MinimumLength = 1)]
         public string EnterprisePayload { get; set; }
 
         /// <summary>
